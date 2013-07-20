d3679e06f85efc998e94d035f6dce41727652774
Waylon <Orlof>
2013-07-19 23:58:52 -0400

    ??
    other countries

---
+++ default/defaultd/DistanceConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter_Lab_
+{
+    public class DistanceConverter : IUnitConverter 
+    {
+        public string FromUnit { get; set; }
+        public string ToUnit { get; set; }
+
+        public DistanceConverter(string fromUnit, string toUnit)
+        {
+            FromUnit = fromUnit;
+            ToUnit = toUnit;
+        }
+
+        public decimal Convert(decimal input)
+        {
+            if (FromUnit == "KM" && ToUnit == "MI")
+                return input * 0.62137M;
+
+            if (FromUnit == "MI" && ToUnit == "KM")
+                return input*1.60934M;
+            
+            return 0;
+            // ...
+        }
+    }
+}
---
+++ default/defaultd/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConverter_Lab_
+{
+    class Program
+    {
+        // miles to kilometers
+        // celsius to farenheit
+        // gallon to liter
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a type to convert from: ");
+            string fromInput = Console.ReadLine();
+            
+            Console.WriteLine("Enter A type to conever to: ");
+            string toInput = Console.ReadLine();
+
+            Console.WriteLine("Enter a Number: ");
+            string howMany = Console.ReadLine();
+            decimal numberToConvert = decimal.Parse(howMany);
+
+            IUnitConverter myConverter = GetConverter(fromInput, toInput);
+            Console.WriteLine("Converted: {0:N2}",myConverter.Convert(numberToConvert));
+            Console.ReadLine();
+        }
+
+        static IUnitConverter GetConverter(string fromUnit, string toUnit)
+        {
+            switch (fromUnit)
+            {
+                case "KM":
+                case "MI":
+                case "KT":
+                return new DistanceConverter(fromUnit, toUnit);
+                    break;
+            }
+            switch (fromUnit)
+            {
+                case "F":
+                case "C":
+                    return new TemperatureConverter(fromUnit, toUnit);
+                    break;
+            }
+            return null;
+        }
+    }
+}
